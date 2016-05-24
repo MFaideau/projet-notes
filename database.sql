3f9b9dc8a869a91c1534e6f141bb9064bbb2f928
Antoine Goelzer <antoine.goelzer@isen-lille.fr>
2016-05-24 09:20:51 +0200

    Correction des tables "Utilisateur" et ajout des ID pour "Type_eval" et "Evaluation"

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 23 Mai 2016 à 17:41
+-- Généré le :  Mar 24 Mai 2016 à 07:19
 -- Version du serveur :  5.7.9
 -- Version de PHP :  5.6.16
 
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `competence`;
 CREATE TABLE IF NOT EXISTS `competence` (
-  `ID_competence` int(11) NOT NULL AUTO_INCREMENT,
-  `Nom_competence` int(11) NOT NULL COMMENT 'Exemple : Signaux et systèmes',
-  PRIMARY KEY (`ID_competence`),
-  UNIQUE KEY `ID_competence` (`ID_competence`)
+  `ID_Competence` int(5) NOT NULL AUTO_INCREMENT,
+  `Nom_Competence` varchar(55) NOT NULL COMMENT 'Exemple : Signaux et systèmes',
+  PRIMARY KEY (`ID_Competence`),
+  UNIQUE KEY `ID_competence` (`ID_Competence`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `competence` (
 
 DROP TABLE IF EXISTS `competencecursus`;
 CREATE TABLE IF NOT EXISTS `competencecursus` (
-  `ID_Cursus` int(3) NOT NULL,
-  `ID_Competence` int(3) NOT NULL,
+  `ID_Cursus` int(5) NOT NULL,
+  `ID_Competence` int(5) NOT NULL,
   PRIMARY KEY (`ID_Cursus`,`ID_Competence`),
   KEY `ID_Competence` (`ID_Competence`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -56,13 +56,13 @@ CREATE TABLE IF NOT EXISTS `competencecursus` (
 
 DROP TABLE IF EXISTS `cours`;
 CREATE TABLE IF NOT EXISTS `cours` (
-  `ID_cours` int(4) NOT NULL AUTO_INCREMENT,
-  `ID_competence` int(3) NOT NULL,
-  `Nom_cours` varchar(55) NOT NULL COMMENT 'Exemple : Transformations',
+  `ID_Cours` int(5) NOT NULL AUTO_INCREMENT,
+  `ID_Competence` int(5) NOT NULL,
+  `Nom_Cours` varchar(55) NOT NULL COMMENT 'Exemple : Transformations',
   `Credits_Cours` float NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID_cours`),
-  UNIQUE KEY `ID_cours` (`ID_cours`),
-  UNIQUE KEY `ID_competence` (`ID_competence`)
+  PRIMARY KEY (`ID_Cours`),
+  UNIQUE KEY `ID_cours` (`ID_Cours`),
+  UNIQUE KEY `ID_competence` (`ID_Competence`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `cours` (
 
 DROP TABLE IF EXISTS `cursus`;
 CREATE TABLE IF NOT EXISTS `cursus` (
-  `ID_Cursus` int(3) NOT NULL AUTO_INCREMENT COMMENT 'Exemple : 0 = CIR3 / 1 = CSI-U3 / 2 = CSI3',
+  `ID_Cursus` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Exemple : 0 = CIR3 / 1 = CSI-U3 / 2 = CSI3',
   `Nom_Cursus` varchar(55) NOT NULL COMMENT 'Exemple : CSI3',
   PRIMARY KEY (`ID_Cursus`),
   UNIQUE KEY `ID_Cursus` (`ID_Cursus`)
@@ -96,13 +96,14 @@ INSERT INTO `cursus` (`ID_Cursus`, `Nom_Cursus`) VALUES
 
 DROP TABLE IF EXISTS `epreuve`;
 CREATE TABLE IF NOT EXISTS `epreuve` (
-  `ID_epreuve` int(5) NOT NULL AUTO_INCREMENT,
-  `Nom_type` varchar(55) NOT NULL,
-  `ID_etudiant` varchar(55) NOT NULL,
+  `ID_Epreuve` int(5) NOT NULL AUTO_INCREMENT,
+  `ID_Type` int(5) NOT NULL,
+  `ID_Etudiant` varchar(55) NOT NULL,
   `Note` float NOT NULL,
-  `Coeff_epreuve` float NOT NULL,
-  PRIMARY KEY (`ID_epreuve`),
-  UNIQUE KEY `ID_etudiant` (`ID_etudiant`)
+  `Coef_Epreuve` float NOT NULL,
+  PRIMARY KEY (`ID_Epreuve`),
+  UNIQUE KEY `ID_etudiant` (`ID_Etudiant`),
+  UNIQUE KEY `ID_Type` (`ID_Type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -113,10 +114,11 @@ CREATE TABLE IF NOT EXISTS `epreuve` (
 
 DROP TABLE IF EXISTS `etudiant`;
 CREATE TABLE IF NOT EXISTS `etudiant` (
-  `ID_etudiant` varchar(55) NOT NULL,
-  `ID_cursus` int(3) NOT NULL,
-  PRIMARY KEY (`ID_etudiant`),
-  UNIQUE KEY `ID_cursus` (`ID_cursus`)
+  `ID_Etudiant` varchar(55) NOT NULL,
+  `ID_Cursus` int(5) NOT NULL,
+  `Mail` varchar(55) NOT NULL,
+  PRIMARY KEY (`ID_Etudiant`),
+  UNIQUE KEY `ID_cursus` (`ID_Cursus`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -127,12 +129,13 @@ CREATE TABLE IF NOT EXISTS `etudiant` (
 
 DROP TABLE IF EXISTS `evaluation`;
 CREATE TABLE IF NOT EXISTS `evaluation` (
-  `Nom_eval` varchar(100) NOT NULL COMMENT 'Exemple : Théorie',
-  `ID_cours` int(4) NOT NULL,
-  `Coef_eval` float NOT NULL,
-  PRIMARY KEY (`Nom_eval`),
-  UNIQUE KEY `Nom_eval` (`Nom_eval`),
-  UNIQUE KEY `ID_cours` (`ID_cours`)
+  `ID_Eval` int(5) NOT NULL AUTO_INCREMENT,
+  `ID_Cours` int(5) NOT NULL,
+  `Nom_Eval` varchar(55) NOT NULL,
+  `Coef_Eval` float NOT NULL,
+  PRIMARY KEY (`ID_Eval`),
+  UNIQUE KEY `Nom_eval` (`ID_Eval`),
+  UNIQUE KEY `ID_cours` (`ID_Cours`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -143,10 +146,11 @@ CREATE TABLE IF NOT EXISTS `evaluation` (
 
 DROP TABLE IF EXISTS `type_eval`;
 CREATE TABLE IF NOT EXISTS `type_eval` (
-  `Nom_type` varchar(55) NOT NULL COMMENT 'Exemple : Interrogation',
-  `Nom_eval` varchar(55) NOT NULL,
-  `Coeff_type_eval` float NOT NULL,
-  PRIMARY KEY (`Nom_type`)
+  `ID_Type` int(5) NOT NULL AUTO_INCREMENT,
+  `ID_Eval` int(5) NOT NULL,
+  `Nom_Type` varchar(55) NOT NULL,
+  `Coef_Type_Eval` float NOT NULL,
+  PRIMARY KEY (`ID_Type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -158,12 +162,10 @@ CREATE TABLE IF NOT EXISTS `type_eval` (
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Mail` varchar(55) NOT NULL,
-  `ID_etudiant` varchar(55) NOT NULL,
-  `Autorité` int(1) NOT NULL,
+  `Autorité` int(5) NOT NULL,
   `Nom` varchar(55) NOT NULL,
   `Prénom` varchar(55) NOT NULL,
-  PRIMARY KEY (`Mail`),
-  UNIQUE KEY `ID_etudiant` (`ID_etudiant`)
+  PRIMARY KEY (`Mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -175,37 +177,38 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 --
 ALTER TABLE `competencecursus`
   ADD CONSTRAINT `competencecursus_ibfk_1` FOREIGN KEY (`ID_Cursus`) REFERENCES `cursus` (`ID_Cursus`),
-  ADD CONSTRAINT `competencecursus_ibfk_2` FOREIGN KEY (`ID_Competence`) REFERENCES `competence` (`ID_competence`);
+  ADD CONSTRAINT `competencecursus_ibfk_2` FOREIGN KEY (`ID_Competence`) REFERENCES `competence` (`ID_Competence`);
 
 --
 -- Contraintes pour la table `cours`
 --
 ALTER TABLE `cours`
-  ADD CONSTRAINT `cours_ibfk_1` FOREIGN KEY (`ID_competence`) REFERENCES `competence` (`ID_competence`);
+  ADD CONSTRAINT `cours_ibfk_1` FOREIGN KEY (`ID_Competence`) REFERENCES `competence` (`ID_Competence`);
 
 --
 -- Contraintes pour la table `epreuve`
 --
 ALTER TABLE `epreuve`
-  ADD CONSTRAINT `epreuve_ibfk_1` FOREIGN KEY (`ID_etudiant`) REFERENCES `etudiant` (`ID_etudiant`);
+  ADD CONSTRAINT `epreuve_ibfk_1` FOREIGN KEY (`ID_Etudiant`) REFERENCES `etudiant` (`ID_Etudiant`),
+  ADD CONSTRAINT `epreuve_ibfk_2` FOREIGN KEY (`ID_Type`) REFERENCES `type_eval` (`ID_Type`);
 
 --
 -- Contraintes pour la table `etudiant`
 --
 ALTER TABLE `etudiant`
-  ADD CONSTRAINT `etudiant_ibfk_1` FOREIGN KEY (`ID_cursus`) REFERENCES `cursus` (`ID_Cursus`);
+  ADD CONSTRAINT `etudiant_ibfk_1` FOREIGN KEY (`ID_Cursus`) REFERENCES `cursus` (`ID_Cursus`);
 
 --
 -- Contraintes pour la table `evaluation`
 --
 ALTER TABLE `evaluation`
-  ADD CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`ID_cours`) REFERENCES `cours` (`ID_cours`);
+  ADD CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`ID_Cours`) REFERENCES `cours` (`ID_Cours`);
 
 --
--- Contraintes pour la table `utilisateur`
+-- Contraintes pour la table `type_eval`
 --
-ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`ID_etudiant`) REFERENCES `etudiant` (`ID_etudiant`);
+ALTER TABLE `type_eval`
+  ADD CONSTRAINT `type_eval_ibfk_1` FOREIGN KEY (`ID_Type`) REFERENCES `evaluation` (`ID_Eval`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
