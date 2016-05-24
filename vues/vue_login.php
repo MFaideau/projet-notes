da4c522f8391362f2213e644867316c8b7c1a280
bLandais <baudouin.landais@isen-lille.fr>
2016-05-24 22:01:47 +0200

    Alignement vertical du texte dans le header de connexion

--- vues/vue_login.php
+++ vues/vue_login.php
@@ -18,11 +18,11 @@
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <div class="row">
-                        <div class="col-xs-4 col-sm-5">
+                    <div class="row header_login">
+                        <div class="col-xs-4 col-sm-5 header_login_cell">
                             <img src="vues/assets/img/logo_isen.png" alt="Logo ISEN" class="logo"/>
                         </div>
-                        <div class="col-xs-8 col-sm-7">
+                        <div class="col-xs-8 col-sm-7 header_login_cell">
                             <div class="texte_logo">Simulateur de notes</div>
                         </div>
                     </div>
