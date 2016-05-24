a904d08c4c54ec2d0e77ea800177836f32635848
bLandais <baudouin.landais@isen-lille.fr>
2016-05-24 13:42:48 +0200

    Ajotu du bouton et du séparateur

--- vues/vue_login.php
+++ vues/vue_login.php
@@ -13,23 +13,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <div class="container">
-       <div class="row">
-           <div class="col-md-6 col-md-offset-3">
-       	       <div class="panel panel-default">
-       	           <div class="panel-body">
-                       <div class="row">
-                           <div class="col-xs-5 col-sm-5">
-                               <img src="vues/assets/img/logo_isen.png" alt="Logo ISEN" class="logo" />            
-                           </div>
-                           <div class="col-xs-7 col-sm-7">
-                               <div class="texte_logo">Simulateur de notes</div>
-                           </div>
-                        </div>                 
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-5 col-sm-5">
+                            <img src="vues/assets/img/logo_isen.png" alt="Logo ISEN" class="logo"/>
+                        </div>
+                        <div class="col-xs-7 col-sm-7">
+                            <div class="texte_logo">Simulateur de notes</div>
+                        </div>
                     </div>
-                </div> 
-           </div>
-         </div>
+                    <div class="row separator_login"></div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <a href="#Google"><div class="connect_button">Connexion au site</div></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 </body>
 </html>
