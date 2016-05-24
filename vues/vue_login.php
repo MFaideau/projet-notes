ed8b69c8601991fe8474bceb737b4c093092cc05
bLandais <baudouin.landais@isen-lille.fr>
2016-05-24 09:06:10 +0200

    Exemple page de login avec Bootstrap

---
+++ vues/vue_login.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @Auteur: Baudouin Landais
+ * @Description : Fichier de vue pour la page de connexion
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page de connexion</title>
+    <link href="vues/assets/bootstrap/bootstrap.css" rel="stylesheet"/>
+    <link href="vues/assets/style.css" rel="stylesheet"/>
+</head>
+<body>
+    <div class="container">
+        <div class="row login_bloc">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Connexion</h3>
+                    </div>
+                    <div class="panel-body">
+                        Cette page vous permet de vous connecter à la plateforme directement à partir de votre compte Google ISEN
+                        <h3 style="text-align: center;">
+                            <!-- TODO : Créer le formulaire de connexion Google !-->
+                            <b><a href="#FormGoogle">CONNEXION</a></b>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
