dbd9b8a2f7d2cda9019cf4b49f474b80e23804d0
bLandais <baudouin.landais@isen-lille.fr>
2016-05-25 09:10:08 +0200

    Remise en forme MVC

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
 include_once("modeles/sqlConnection.php");
 
-// TODO : Enlever cette include car on ne peut pas include une vue directement
-include_once("vues/vue_login.php");
+include_once("controleurs/login.php");
