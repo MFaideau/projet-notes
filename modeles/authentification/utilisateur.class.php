7e14db40d7eb54c77111c061813f87df37566886
Antoine Goelzer <antoine.goelzer@isen-lille.fr>
2016-05-26 09:05:02 +0200

    Ajout d'authentification

---
+++ modeles/authentification/utilisateur.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ISEN
+ * Date: 25/05/2016
+ * Time: 11:20
+ */
+class Utilisateur
+{
+    private $mail;
+    private $autorite;
+    private $nom;
+    private $prenom;
+
+    function Utilisateur($user)
+    {
+        $this->mail=$user["Mail"];
+        $this->autorite=$user["Autorite"];
+        $this->nom=$user["Nom"];
+        $this->prenom=$user["Prenom"];
+    }
+
+    public function GetMail()
+    {
+        return $this->mail;
+    }
+}
+?>
