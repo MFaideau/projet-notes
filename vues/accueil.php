b01dd9669ed57198e7a3762b94a2291dafac3252
Joël <joel.guillem@isen-lille.fr>
2016-05-26 13:56:54 +0200

    Meilleure division de la page entre menu et accueil

---
+++ vues/accueil.php
@@ -0,0 +1,77 @@
+<!-- Selection entre les Tableaux et les Histogrammes !-->
+<div class="row">
+    <div class="col-md-2 col-md-offset-10 visualisation">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <a href="#tableau_notes">
+                    <span class="glyphicon glyphicon-list-alt icone"></span>
+                </a>
+                <a href="#histogramme_notes">
+                    <span class="glyphicon glyphicon-align-left icone"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Page de données !-->
+
+<div class="row donnees">
+    <div class="panel panel-default">
+        <div class="panel-heading">Relevé de notes</div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Compétences</th>
+                <th>Moyennes</th>
+                <th>ECTS</th>
+                <th>Grades</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">Informatique</th>
+                <td>12.2</td>
+                <td>15</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <th scope="row">Electronique</th>
+                <td>13.4</td>
+                <td>15</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <th scope="row">Management</th>
+                <td>13</td>
+                <td>15</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <th scope="row">Signaux</th>
+                <td>15</td>
+                <td>15</td>
+                <td>A</td>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>Total</th>
+                <th>XX</th>
+                <th>60</th>
+                <th>A</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
+
+<!-- Histogramme de données !-->
+<div>
+
+</div>
+
+</div>
+</div>
+</body>
+</html>
