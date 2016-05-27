6b729fbcb0a7cd4a17c3478360744bb04e530cb1
Joël <joel.guillem@isen-lille.fr>
2016-05-27 10:50:54 +0200

    Création des histogrammes modulables

--- vues/accueil.php
+++ vues/accueil.php
@@ -23,7 +23,7 @@
             <thead>
             <tr>
                 <th>Compétences</th>
-                <th>Moyennes</th>
+                <th>Moyenne</th>
                 <th>ECTS</th>
                 <th>Grades</th>
             </tr>
@@ -67,8 +67,65 @@
 </div>
 
 <!-- Histogramme de données !-->
-<div>
 
+<div class="row donnees">
+    <div class="panel panel-default">
+        <div class="panel-heading">Graphe comparatoire</div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Compétences</th>
+                <th>Histogramme</th>
+                <th>Max</th>
+                <th>Etudiant</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">Informatique</th>
+                <td width="50%">
+                    <div class="histo"></div>
+                </td>
+                <td>16.5</td>
+                <td>15</td>
+            </tr>
+            <tr>
+                <th scope="row">Electronique</th>
+                <td width="50%">
+                    <div class="histo"></div>
+                </td>
+                <td>15.2</td>
+                <td>15.2</td>
+            </tr>
+            <tr>
+                <th scope="row">Management</th>
+                <td width="50%" class="histo_parent">
+                    <div class="histo"><div>
+                </td>
+                <td>17.1</td>
+                <td>13.3</td>
+            </tr>
+            <tr>
+                <th scope="row">Signaux</th>
+                <td width="50%">
+                    <div class="histo"></div>
+                </td>
+                <td>16</td>
+                <td>14.8</td>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>Total</th>
+                <th width="50%">
+                    <div class="histo"></div>
+                </th>
+                <th>16.2</th>
+                <th>13.5</th>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
 </div>
 
 </div>
---
+++ vues/admin/saisie_notes.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @Auteur : bLandais
+ * @Description : Vue pour le panneau d'insertion de notes
+*/
+?>
+
+<div class="panel panel-default saisie_notes panel_cursus">
+    <div class="panel-heading">Choix du Cursus</div>
+    <div class="panel-body">
+        <div class="btn-group btn-group-justified" role="group" aria-label="...">
+            <div class="btn-group" role="group">
+                <button id="cursus_1" type="button" class="btn btn-default">CSI3</button>
+            </div>
+            <div class="btn-group" role="group">
+                <button id="cursus_2" type="button" class="btn btn-default">CSI-U3</button>
+            </div>
+            <div class="btn-group" role="group">
+                <button id="cursus_3" type="button" class="btn btn-default">CIR3</button>
+            </div>
+        </div>
+    </div>
+</div>
