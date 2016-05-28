1a39f1147cd30858d323546c8503f5928ca9dad4
bLandais <baudouin.landais@isen-lille.fr>
2016-05-28 18:14:46 +0200

    Début de la partie dynamique pour ajouter un cursus (sans lien avec la BDD) #2

--- ajax/admin_ajax_orga.php
+++ ajax/admin_ajax_orga.php
@@ -133,7 +133,9 @@ if (isset($_POST['idTypeEval'])) {
 
 if (isset($_POST['nomCursus'])) {
     ?>
+    <div class="btn-group" role="group">
         <button id="orga_cursus_<?php echo $_POST['nomCursus']; ?>" type="button"
                 class="btn btn-default"><?php echo $_POST['nomCursus']; ?></button>
+    </div>
 <?php }
 ?>
