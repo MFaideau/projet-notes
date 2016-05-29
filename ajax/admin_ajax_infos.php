27828d1f3614a6334b76920082c1968363586b44
bLandais <baudouin.landais@isen-lille.fr>
2016-05-29 18:27:07 +0200

    Deuxième refonte du MVC (partie Index <-> Controleurs)

--- ajax/admin_ajax_infos.php
+++ ajax/admin_ajax_infos.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 include_once __DIR__ . '../../modeles/sqlConnection.php';
@@ -8,47 +9,6 @@ include_once __DIR__ . '../../modeles/type_evaluation/typeeval.php';
 
 if (!isset($_SESSION['user'])) {
     die();
-} else {
-    $utilisateur = unserialize($_SESSION['user']);
-    if ($utilisateur->GetAutorite() != 1) {
-        die();
-    }
 }
 
-$user = serialize($_SESSION['user']);
-
-if (isset($_POST['idCursus'])) {
-    $idCursus = $_POST['idCursus'];
-    $cursus = GetCursus(GetCursusList(), $idCursus);
-    if (empty($cursus)) return;
-    else
-        include_once __DIR__ . '../../vues/admin/ajax/saisie/competences_bloc.php';
-}
-if (isset($_POST['idCompetence'])) {
-    $idCompetence = $_POST['idCompetence'];
-    $cours = GetCoursListFromCompetence($idCompetence);
-    if (empty($cours)) return;
-    else
-        include_once __DIR__ . '../../vues/admin/ajax/saisie/cours_bloc.php';
-}
-if (isset($_POST['idCours'])) {
-    $idCours = $_POST['idCours'];
-    $typeEvalList = GetTypeEvalListFromCours($idCours);
-    if (empty($typeEvalList)) return;
-    else
-        include_once __DIR__ . '../../vues/admin/ajax/saisie/types_eval_bloc.php';
-}
-if (isset($_POST['idTypeEval'])) {
-    $idTypeEval = $_POST['idTypeEval'];
-    $epreuveList = GetEpreuveListFromTypeEval($idTypeEval);
-    if (empty($epreuveList)) return;
-    else
-        include_once __DIR__ . '../../vues/admin/ajax/saisie/epreuves_bloc.php';
-}
-if (isset($_POST['idEpreuve'])) {
-    $idUploadEval = $_POST['idEpreuve'];
-    if (empty($idUploadEval)) return;
-    else
-        include_once __DIR__ . '../../vues/admin/ajax/saisie/upload_bloc.php';
-}
-?>
+include_once __DIR__ . './../controleurs/ajax/admin_ajax_infos.php';
