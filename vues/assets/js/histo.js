e310b43b334b9a6ea572467d8177c447bdd73388
Joël <joel.guillem@isen-lille.fr>
2016-05-30 16:09:23 +0200

    Terminaison de la partie codage des histogrammes

--- vues/assets/js/histo.js
+++ vues/assets/js/histo.js
@@ -1,37 +1,32 @@
 function loadBar() {
     var ctx = document.getElementById("myChart").getContext('2d');
-    var BigData = {
+    var dData = function() {
+        return Math.round(Math.random() * 10)
+    };
+
+    var barChartData = {
         labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
-        dataSets: [{
-            label: "Barres",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 1,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+        datasets: [{
+            backgroundColor: "#b62224",
+            borderColor: "rgba(255,0,0,1)",
+            strokeColor: "black",
+            data: [dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData()]
         }]
     };
-    var BarChart = new Chart(ctx, {
+
+    var index = 11;
+    var barChartDemo = new Chart(ctx, {
         type: 'bar',
-        data: BigData,
+        data: barChartData,
+        responsive: true,
+        barValueSpacing: 2,
         options: {
-            // Elements options apply to all of the options unless overridden in a dataset
-            // In this case, we are setting the border of each bar to be 2px wide and green
-            elements: {
-                rectangle: {
-                    borderWidth: 2,
-                    borderColor: 'rgb(0, 255, 0)',
-                    borderSkipped: 'bottom'
-                }
-            },
-            responsive: true,
-            legend: {
-                position: 'top',
-            },
             title: {
                 display: true,
-                text: 'Dispersion des notes'
+                text: 'Diagramme de notes'
+            },
+            legend: {
+                display: false
             }
         }
     });
