39fb1ee2018fbb6ec93ab202a1501b4b8e83e234
Joël <joel.guillem@isen-lille.fr>
2016-05-30 14:50:46 +0200

    Codage de la partie histogramme en bâtons sans l'échelle

--- vues/ajax/batons_bloc.php
+++ vues/ajax/batons_bloc.php
@@ -2,5 +2,8 @@
 <!-- Histogramme en bâtons de données !-->
 
 <div class="row donnees donnees_batons">
-    
+    <div class="panel panel-default">
+        <div class="panel-heading">Répartition dans la promo</div>
+        <canvas id="myChart" width="100%" height="60%"></canvas>
+    </div>
 </div>
