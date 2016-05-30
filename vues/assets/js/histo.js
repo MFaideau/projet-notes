1b3d4b1cf391dc210a72fbab3947177e4e27b020
Joël <joel.guillem@isen-lille.fr>
2016-05-30 17:16:35 +0200

    Terminaison de la partie codage des histogrammes#3

--- vues/assets/js/histo.js
+++ vues/assets/js/histo.js
@@ -8,13 +8,11 @@ function loadBar() {
         labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
         datasets: [{
             backgroundColor: "#b62224",
-            borderColor: "rgba(255,0,0,1)",
             strokeColor: "black",
             data: [dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData()]
         }]
     };
 
-    var index = 11;
     var barChartDemo = new Chart(ctx, {
         type: 'bar',
         data: barChartData,
