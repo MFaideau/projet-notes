fe663c5e21cbd5f580360dc95d1fd7e1df5dfbd8
Joël <joel.guillem@isen-lille.fr>
2016-05-30 15:36:01 +0200

    Codage de la partie histogramme en bâtons et correction de 2-3 erreurs Manque les rectangles sur l'histogramme

--- vues/ajax/batons_bloc.php
+++ vues/ajax/batons_bloc.php
@@ -3,7 +3,7 @@
 
 <div class="row donnees donnees_batons">
     <div class="panel panel-default">
-        <div class="panel-heading">Répartition dans la promo</div>
+        <div class="panel-heading">Répartition de la promo</div>
         <canvas id="myChart" width="100%" height="60%"></canvas>
     </div>
 </div>
--- vues/assets/js/histo.js
+++ vues/assets/js/histo.js
@@ -1,19 +1,38 @@
 function loadBar() {
     var ctx = document.getElementById("myChart").getContext('2d');
     var BigData = {
-        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
         dataSets: [{
-            label: "GrapheBarres",
-            backgroundColor: "rgba(255,99,132,0.2",
+            label: "Barres",
+            backgroundColor: "rgba(255,99,132,0.2)",
             borderColor: "rgba(255,99,132,1)",
             borderWidth: 1,
             hoverBackgroundColor: "rgba(255,99,132,0.4)",
             hoverBorderColor: "rgba(255,99,132,1)",
-            data: [0, 0, 0, 0, 0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0],
+            data: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
         }]
-    }
+    };
     var BarChart = new Chart(ctx, {
         type: 'bar',
-        data: BigData
+        data: BigData,
+        options: {
+            // Elements options apply to all of the options unless overridden in a dataset
+            // In this case, we are setting the border of each bar to be 2px wide and green
+            elements: {
+                rectangle: {
+                    borderWidth: 2,
+                    borderColor: 'rgb(0, 255, 0)',
+                    borderSkipped: 'bottom'
+                }
+            },
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Dispersion des notes'
+            }
+        }
     });
 }
