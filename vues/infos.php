c005c103f342daaf690cf34b00e72c04cbefe150
Maxence <maxence.faideau@isen-lille.fr>
2016-05-30 15:53:57 +0200

    Affichage des bonnes fonctionnalités en fonction de l"autorité

---
+++ vues/infos.php
@@ -0,0 +1,14 @@
+<div class="col-lg-3 identite">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row"><span><strong><?php echo $user->GetPrenom() . ' ' . $user->GetNom(); ?></strong></span>
+            </div>
+            <div class="row"><span><?php echo $user->GetCursusTexte(); ?></span></div>
+            <div class="row">
+                <a href="index.php?action=disconnect">
+                    <div class="disconnect">Se déconnecter</div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
