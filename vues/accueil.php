9930be72c5abd22bcae3c298007e16e900cba5ca
Maxence <maxence.faideau@isen-lille.fr>
2016-05-30 15:20:30 +0200

    Création d'un menu_rapide.php (tous les petits glyphicon) et donc d'une condition pour l'afficher ou non en fonction de l'autorité

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,21 +1,4 @@
-<!-- Selection entre les Tableaux et les Histogrammes !-->
-<div class="row">
-    <div class="col-md-2.5 col-md-offset-9.5 visualisation">
-        <div class="panel panel-default">
-            <div class="panel-body ">
-                <a href="#tableau_notes" class="tableaux_logo">
-                    <span class="glyphicon glyphicon-list-alt icone"></span>
-                </a>
-                <a href="#histogramme_notes" class="histo_logo">
-                    <span class="glyphicon glyphicon-align-left icone"></span>
-                </a>
-                <a href="#histogramme_commun" class="histo_commun">
-                    <span class="glyphicon glyphicon-stats icone"></span>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 <!-- Page de données !-->
 <div class="row donnees donnees_tableaux">
