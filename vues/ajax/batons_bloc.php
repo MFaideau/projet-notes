e8e59bc24a49fb7b5c957f3d0904fe9f97b5a99a
Maxence <maxence.faideau@isen-lille.fr>
2016-06-01 10:26:45 +0200

    Création menu histogramme commun

--- vues/ajax/batons_bloc.php
+++ vues/ajax/batons_bloc.php
@@ -1,9 +1,79 @@
 
 <!-- Histogramme en bâtons de données !-->
 
-<div class="row donnees donnees_batons">
+<div class="row donnees donnees_histo">
     <div class="panel panel-default">
-        <div class="panel-heading">Répartition de la promo</div>
-        <canvas id="myChart" width="100%" height="40%"></canvas>
-    </div>
-</div>
+        <div class="panel-heading">Histogramme Commun</div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Compétences</th>
+                <th>ECTS</th>
+                <th>Max</th>
+                <th>Etudiant</th>
+                <th>Grade</th>
+                <th>Hist.</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">Informatique</th>
+                <td>13</td>
+                <td>16.5</td>
+                <td>15</td>
+                <td>A</td>
+                <td>
+                    <a href="#histogramme_commun" class="histo_commun">
+                        <span class="glyphicon glyphicon-stats icone"></span>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+            <tr>
+                <th scope="row">Electronique</th>
+                <td>13</td>
+                <td>16.5</td>
+                <td>15</td>
+                <td>A</td>
+                <td>
+                    <a href="#histogramme_commun" class="histo_commun">
+                        <span class="glyphicon glyphicon-stats icone"></span>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">Management</th>
+                <td>13</td>
+                <td>16.5</td>
+                <td>15</td>
+                <td>A</td>
+                <td>
+                    <a href="#histogramme_commun" class="histo_commun">
+                        <span class="glyphicon glyphicon-stats icone"></span>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">Signaux</th>
+                <td>13</td>
+                <td>16.5</td>
+                <td>15</td>
+                <td>A</td>
+                <td>
+                    <a href="#zoom_competence_1" class="histo_commun">
+                        <span class="glyphicon glyphicon-stats icone"></span>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">Total</th>
+                <td>13</td>
+                <td>16.5</td>
+                <td>15</td>
+                <td>A</td>
+                <td>
+                    <a href="#histogramme_commun" class="histo_commun">
+                        <span class="glyphicon glyphicon-stats icone"></span>
+                    </a>
+                </td>
+            </tr>
