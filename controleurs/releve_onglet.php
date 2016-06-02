d300abb99305e786fa83617562012b01e1c433b7
Maxence <maxence.faideau@isen-lille.fr>
2016-06-02 14:19:56 +0200

    Correction petit bug#2

--- controleurs/releve_onglet.php
+++ controleurs/releve_onglet.php
@@ -4,3 +4,4 @@ include_once (__DIR__ . '../../modeles/sqlConnection.php');
 include_once (__DIR__ . '../../vues/menu.php');
 include_once (__DIR__ . '../../vues/menu_rapide.php');
 include_once (__DIR__ . '../../vues/ajax/tableaux_bloc.php');
+include_once (__DIR__ . '../../vues/footer.php');
--- controleurs/simulateur_onglet.php
+++ controleurs/simulateur_onglet.php
@@ -2,5 +2,6 @@
 
 include_once (__DIR__ . '../../modeles/sqlConnection.php');
 include_once (__DIR__ . '../../vues/menu.php');
-include_once (__DIR__ . '../../vues/menu_rapide.php');
+
 include_once (__DIR__ . '../../vues/simulateur_onglet.php');
+include_once (__DIR__ . '../../vues/footer.php');
