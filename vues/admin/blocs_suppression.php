bfe5f46d7b89c2f51de604a0fd1a0620c2da0b4c
Antoine Goelzer <antoine.goelzer@isen-lille.fr>
2016-06-03 10:37:59 +0200

    Avertissements suppression faits jusque type éval inclus.

--- vues/admin/blocs_suppression.php
+++ vues/admin/blocs_suppression.php
@@ -73,3 +73,46 @@
         </div>
     </div>
 </div>
+
+<!-- Fenêtre de confirmation pour la suppression des éval !-->
+<div class="modal fade" id="verifDeleteEval" tabindex="-1" role="dialog" aria-labelledby="verifDeleteEval">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form id="verifDeleteEval">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Validation de la suppression</h4>
+                </div>
+                <div class="modal-body">
+                    Êtes-vous sûr de vouloir supprimer l'évaluation selectionnée ?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Non</button>
+                    <button type="submit" class="btn btn-primary">Oui</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Fenêtre de confirmation pour la suppression des type éval !-->
+<div class="modal fade" id="verifDeleteTypeEval" tabindex="-1" role="dialog" aria-labelledby="verifDeleteTypeEval">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form id="verifDeleteTypeEval">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Validation de la suppression</h4>
+                </div>
+                <div class="modal-body">
+                    Êtes-vous sûr de vouloir supprimer le type d'évaluation selectionné ?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Non</button>
+                    <button type="submit" class="btn btn-primary">Oui</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
