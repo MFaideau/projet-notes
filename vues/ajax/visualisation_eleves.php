3b0517741a731d99b1c7ad3d0af6d4eac2dd7260
bLandais <baudouin.landais@isen-lille.fr>
2016-06-05 11:16:35 +0200

    Ajout des boutons pour supprimet ou rajouter un étudiant dans le panneau admin

--- vues/ajax/visualisation_eleves.php
+++ vues/ajax/visualisation_eleves.php
@@ -3,6 +3,7 @@
     <table class="table" data-toggle="table" data-sort-name="nom" data-sort-order="asc">
         <thead>
         <tr>
+            <th data-field="action" data-sortable="false">Action</th>
             <th data-field="nom" data-sortable="true">Nom</th>
             <th data-field="moyenne" data-sortable="true">Moyenne générale</th>
         </tr>
@@ -10,6 +11,7 @@
         <tbody>
         <?php foreach ($listEleves as $eleve) { ?>
             <tr>
+                <td width="2%"><a href="#deleteEtudiant"><span class="glyphicon glyphicon-minus-sign icone"></span></a></td>
                 <td><a data-name="nom" data-value="<?php echo $eleve->GetNom() . $eleve->GetPrenom(); ?>"
                        href="visualisation_eleve.php?id=<?php echo $eleve->GetMail(); ?>">
                         <?php echo $eleve->GetNom() . ' ' . $eleve->GetPrenom(); ?></a></td>
@@ -17,5 +19,11 @@
             </tr>
         <?php } ?>
         </tbody>
+        <tfoot>
+            <tr>
+                <td class="ajoutEtudiant" colspan="3"><span class="glyphicon glyphicon-plus-sign icone"></span>
+                <a href="#ajoutEtudiant">Ajouter un étudiant</a></td>
+            </tr>
+        </tfoot>
     </table>
 </div>
