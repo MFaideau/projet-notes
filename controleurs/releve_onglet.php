8e8d77948a23edcd215f73a182ba70071ea4af6d
bLandais <baudouin.landais@isen-lille.fr>
2016-06-06 09:35:24 +0200

    Merge branch 'AjoutEpreuve' of https://github.com/bLandais/projet-notes into Visualisation_eleves
    # Conflicts:
    #	vues/menu.php

--- controleurs/releve_onglet.php
+++ controleurs/releve_onglet.php
@@ -1,7 +1,16 @@
 <?php
 
 include_once (__DIR__ . '../../modeles/sqlConnection.php');
-include_once (__DIR__ . '../../vues/menu.php');
-include_once (__DIR__ . '../../vues/menu_rapide.php');
-include_once (__DIR__ . '../../vues/ajax/tableaux_bloc.php');
-include_once (__DIR__ . '../../vues/footer.php');
+include_once('modeles/sqlConnection.php');
+include_once ('./modeles/authentification/utilisateur.class.php');
+
+if (!isset($_SESSION['user'])) {
+    header('Location: index.php');
+    die(); }
+else {
+    $user = unserialize($_SESSION['user']);
+    include_once(__DIR__ . '../../vues/menu.php');
+    include_once(__DIR__ . '../../vues/menu_rapide.php');
+    include_once(__DIR__ . '../../vues/ajax/tableaux_bloc.php');
+    include_once(__DIR__ . '../../vues/footer.php');
+}
