7418bf1ad63892f765a9d90265284ffe396013c4
Maxence <maxence.faideau@isen-lille.fr>
2016-06-07 10:30:03 +0200

    Merge branch 'master' of https://github.com/bLandais/projet-notes into boutons_comp
    # Conflicts:
    #	vues/assets/admin_script_orga.js

---
+++ controleurs/simulation.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once (__DIR__ . '../../modeles/sqlConnection.php');
+include_once ('./modeles/authentification/utilisateur.class.php');
+
+if (!isset($_SESSION['user'])) {
+    header('Location: index.php');
+    die(); }
+else {
+    $user = unserialize($_SESSION['user']);
+    include_once(__DIR__ . '../../vues/menu.php');
+    include_once(__DIR__ . '../../vues/simulation.php');
+    include_once(__DIR__ . '../../vues/footer.php');
+}
+
