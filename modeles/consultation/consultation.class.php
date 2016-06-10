fc3e8e51a0ab2db3588ebd497188d6a2752ea576
Antoine Goelzer <antoine.goelzer@isen-lille.fr>
2016-06-10 08:43:37 +0200

    Début consultation

---
+++ modeles/consultation/consultation.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ISEN
+ * Date: 10/06/2016
+ * Time: 08:42
+ */
+class Consultation
+{
+    private $idEtudiant;
+    private $mailConsultant;
+    private $nombreVues;
+
+    
+}
