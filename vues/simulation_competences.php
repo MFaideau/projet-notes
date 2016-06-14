b1b7ec2a3c6241e48dc87bae180f5abcc4d7b0d4
bLandais <baudouin.landais@isen-lille.fr>
2016-06-14 13:46:34 +0200

    Simulateur

--- vues/simulation_competences.php
+++ vues/simulation_competences.php
@@ -1,26 +1,52 @@
-<!-- Simulateur Manuel !-->
-<div class="row donnees donnees_tableaux panel_simu_comp">
+<!-- Page de données !-->
+<div class="row donnees donnees_tableaux">
     <div class="panel panel-default">
-        <div class="panel-heading">Simulateur Manuel</div>
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Moyenne</th>
-                <th>Coefficient</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($competenceList as $competence) { ?>
-                <tr>
-                    <td><a id="simu_comp_<?php echo $competence->GetId(); ?>"><?php echo $competence->GetNom(); ?></a></td>
-                    <td><?php echo GetMoyenneFromCompetence($competence->GetId(), GetEtudiant($user)->GetId()); ?></td>
-                    <td><?php echo "12"; ?></td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
+        <div class="panel-heading">Relevé de notes</div>
+        <div class="panel_listing">
+            <table class="table" data-toggle="table">
+                <thead>
+                    <tr>
+                        <th>Compétences</th>
+                        <th>ECTS</th>
+                        <th>Moyenne</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($competences as $competence) { ?>
+                        <tr>
+                            <td class="lien_tableau" scope="row">
+                                <a class="lien_tableau" href="simulateur_onglet.php?idCompetence=<?php echo $competence->GetId(); ?>">
+                                    <b><?php echo $competence->GetNom(); ?></b>
+                                </a>
+                            </td>
+                            <td><?php echo $competence->GetCredits(); ?></td>
+                            <td>
+                                <?php
+                                $note_etudiant = round(GetMoyenneFromCompetence($competence->GetId(), GetEtudiant($user)->GetId()), 2);
+                                echo $note_etudiant;
+                                ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+                <?php
+                if (!empty($competences)) {
+                    ?>
+                    <tfoot>
+                        <tr>
+                            <td><b>Total</b></td>
+                            <td><b><?php echo $cursus->GetCredits(); ?></b></td>
+                            <td><b>
+                                    <?php
+                                    $note_etudiant = round(GetMoyenneFromCursus(GetEtudiant($user)->GetCursus()->GetId(), GetEtudiant($user)->GetId()), 2);
+                                    echo $note_etudiant;
+                                    ?>
+                                </b>
+                            </td>
+                        </tr>
+                    </tfoot>
+                <?php } ?>
+            </table>
+        </div>
     </div>
 </div>
-
-
