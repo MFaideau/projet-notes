ea17c4f83cea04ce6c4b889b6377029524c9f8d0
Antoine Goelzer <antoine.goelzer@isen-lille.fr>
2016-06-20 14:47:03 +0200

    Début de l'exportation de la BDD

--- modeles/sqlConnection.php
+++ modeles/sqlConnection.php
@@ -4,7 +4,7 @@
  * @Date:   21/05/2016
  * @Desc:   Connect to the SQL database
  */
-
+include_once("sqlExport.php");
 try {
     $bdd = new PDO('mysql:host=localhost;dbname=projet_notes', 'root', '');
     $bdd->exec("SET NAMES 'UTF8'");
