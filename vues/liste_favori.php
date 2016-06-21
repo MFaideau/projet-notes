58c7f395457faaac266fa5637dfe06dd2d2c7a9c
bLandais <baudouin.landais@isen-lille.fr>
2016-06-21 15:50:37 +0200

    Lien dans le TOP10 pour visualiser directement l'étudiant

--- vues/liste_favori.php
+++ vues/liste_favori.php
@@ -14,7 +14,7 @@
             $etudiant = GetUserFromEtudiant($eleve->GetId());
             if(isset($etudiant)) { ?>
             <tr>
-                <td><?php echo $etudiant->GetNom() . ' ' . $etudiant->GetPrenom(); ?></td>
+                <td><a href="visualisation_eleve.php?id=<?php echo $etudiant->GetMail(); ?>"><?php echo $etudiant->GetNom() . ' ' . $etudiant->GetPrenom(); ?></a></td>
                 <td><?php echo round(GetMoyenneFromCursus(GetEtudiant($etudiant)->GetCursus()->GetId(), $eleve->GetId()),2); ?></td>
             </tr>
         <?php } } ?>
